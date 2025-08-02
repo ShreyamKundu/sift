847b606f9f30d9076c11d72dee985574f3d9eb73
Shreyam Kundu <shreyamkundu04@gmail.com>
2025-08-02 20:22:47 +0530

    feat: Implement recursive directory scanning

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"path/filepath"
+)
 
 func main() {
 	fmt.Print("Welcome to Sift - Your Smart File Organizer!\n")
+
+	const sourceDir = "test-folder"
+
+	fmt.Printf("Scanning directory: %s\n", sourceDir)
+
+	err := filepath.WalkDir(sourceDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !d.IsDir() {
+			fmt.Printf("Found file: %s\n", path)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalf("Error walking the path %q: %v\n", sourceDir, err)
+	}
 }
