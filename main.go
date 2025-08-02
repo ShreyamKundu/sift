96b9f605ef9589dee00916f5241c7ef96bfea350
Shreyam Kundu <shreyamkundu04@gmail.com>
2025-08-02 20:39:30 +0530

    feat: Implement basic rule engine for file classification

--- main.go
+++ main.go
@@ -5,22 +5,51 @@ import (
 	"io/fs"
 	"log"
 	"path/filepath"
+	"strings"
 )
+
+// ruleMap defines the mapping from file extension to destination folder.
+var ruleMap = map[string]string{
+	".jpg":  "Images",
+	".jpeg": "Images",
+	".png":  "Images",
+	".gif":  "Images",
+
+	".pdf":  "Documents",
+	".docx": "Documents",
+	".txt":  "Documents",
+
+	".mp3": "Audio",
+	".wav": "Audio",
+	".mp4": "Videos",
+}
 
 func main() {
 	fmt.Print("Welcome to Sift - Your Smart File Organizer!\n")
 
 	const sourceDir = "test-folder"
 
-	fmt.Printf("Scanning directory: %s\n", sourceDir)
+	fmt.Printf("\nScanning directory: %s\n\n", sourceDir)
 
 	err := filepath.WalkDir(sourceDir, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
-		if !d.IsDir() {
-			fmt.Printf("Found file: %s\n", path)
+		// Skip directories
+		if d.IsDir() {
+			return nil
+		}
+
+		//Get the file extension
+		ext := strings.ToLower(filepath.Ext(path))
+
+		destFolder, ok := ruleMap[ext]
+
+		if ok {
+			fmt.Printf("File: %s -> belongs in: %s\n", path, destFolder)
+		} else {
+			fmt.Printf("File: %s -> belongs in: Others\n", path)
 		}
 
 		return nil
